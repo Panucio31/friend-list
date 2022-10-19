fdd36b19df38093504271edafb69cda5cbe15a18
Panucio31 <m.januska1990@gmail.com>
2022-10-19 19:25:14 +0300

    feat: :sparkles: Created Friend Card Components and input fields

---
+++ src/components/AddFriend.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class AddFriend extends Component {
+  render() {
+    return (
+      <div className="formContainer">
+        <form action="send">
+          <input type="text" name="name" placeholder="Name" />
+
+          <input type="text" name="surname" placeholder="Surname" />
+
+          <input type="number" name="age" placeholder="Age" />
+
+          <input type="text" name="city" placeholder="City" />
+        </form>
+        <button className="btn">Submit</button>
+      </div>
+    );
+  }
+}
+
+export default AddFriend;
---
+++ src/components/FriendCard.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class FriendCard extends Component {
+  friends = [
+    { firstName: "John", lastName: "Smith", age: 32, city: "Kaunas" },
+    { firstName: "Maria", lastName: "Hudghes", age: 28, city: "Siauliai" },
+    { firstName: "Thomas", lastName: "Muiller", age: 33, city: "Vilnius" },
+  ];
+
+  render() {
+    return (
+    this.friends.map((friend)=>
+      <div className="container">
+        <div className="fNameLName">
+          {friend.firstName} {friend.lastName}
+        </div>
+        <div className="restInfo">
+        Age: {friend.age}, From: {friend.city}.
+        </div>
+      </div>
+    )
+    )
+  }
+}
+
+export default FriendCard;
